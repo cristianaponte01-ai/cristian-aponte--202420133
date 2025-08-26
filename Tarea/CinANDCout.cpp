aa6c3b8476168f90676c70322416c294d1b3efd1
cristianaponte01-ai <cristian.aponte01@uptc.edu.co>
2025-08-26 16:54:26 +0000

    first commit

---
+++ Tarea/CinANDCout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "hola mundo" << endl;
+    cout << "mi nombre es c++" << endl;
+    return 0;
+}
+
---
+++ Tarea/TAREA2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nombre;
+
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+
+    cout << "Hola, " << nombre << "! Bienvenido a C++, hoy vamo' a estresarnos" << endl;
+
+    return 0;
+}
